beb6acf5ed633a9f336b20a99159eaffba3ef1b2
Ethan Low <ethanlow223@yahoo.com>
2019-11-20 15:36:46 -0800

    changed server folder

--- server/app.py
+++ server/app.py
@@ -1,18 +1,13 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from config import DB_USERNAME, DB_PASSWORD, DB_NAME
+from api.ping_handler import ping_handler
+from api.home_handler import home_handler
+from models import db
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://{}:{}@localhost/{}'.format(DB_USERNAME, DB_PASSWORD, DB_NAME)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db.SQLAlchemy(app)
-
-from flask_bcrypt import Bcrypt
-bcrypt = Bcrypt(app)
-
-from api.ping_handler import ping_handler
-from api.home_handler import home_handler
+db.init_app(app)
 
 app.register_blueprint(home_handler)
 app.register_blueprint(ping_handler)
-
