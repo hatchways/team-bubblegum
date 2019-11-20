acc00c84562d4189b6157a72103747574dff91fb
Isabel Tang <isabeltang220@gmail.com>
2019-11-20 18:32:44 -0500

    Mapped receipt class to table for database

--- server/controller/receipt_controller.py
+++ server/controller/receipt_controller.py
@@ -1,12 +1,24 @@
+from flask import Blueprint, request
+from ..models import db, Receipt
 from datetime import date
 
-class ReceiptController:
+receipt_controller = Blueprint('receipt_controller',
+                               __name__, url_prefix='/receipts')
 
-    def __init__(self, model, view):
-        self.model = model
-        self.view = view
 
-    def create_receipt(self, amount, title, receipt_date, pic_url,
-                       category=None, date_created=date.today()):
-        self.model.create_receipt(amount, title, receipt_date, pic_url,
-                                  category, date_created)
+@receipt_controller.route('', methods=['POST'])
+def create():
+    receipt_data = request.json
+
+    amount = receipt_data['amount']
+    title = receipt_data['title']
+    category = receipt_data['category']
+    receipt_date = receipt_data['receipt_date']
+    date_created = receipt_data['date_created']
+    pic_url = receipt_data['pic_url']
+
+    receipt = Receipt(amount=amount, title=title, category=category,
+                      receipt_date=receipt_date, date_created=date_created,
+                      pic_url=pic_url)
+    db.session.add(receipt)
+    db.session.commit()
--- server/models.py
+++ server/models.py
@@ -1,3 +1,27 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import date
 
 db = SQLAlchemy()
+
+
+class Receipt(db.Model):
+    __tablename__ = 'receipt'
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    amount = db.Column(db.Numeric(12,2), nullable=False)
+    title = db.Column(db.String(100), nullable=False)
+    category = db.Column(db.String)
+    receipt_date = db.Column(db.Date, nullable=False)
+    date_created = db.Column(db.Date, default=date.today())
+    pic_url = db.Column(db.String, nullable=False)
+
+    def __init__(self, amount, title, receipt_date, pic_url,
+                 category=None, date_created=date.today()):
+        self.amount = amount
+        self.title = title
+        self.receipt_date = receipt_date
+        self.pic_url = pic_url
+        self.category = category
+        self.date_created = date_created
+
