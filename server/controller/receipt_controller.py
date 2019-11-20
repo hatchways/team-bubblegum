942025b29ad7d53348fe00fa8152719454d647e2
Isabel Tang <isabeltang220@gmail.com>
2019-11-20 12:23:37 -0500

    Modified receipt controller constructor and added method to create receipt

--- server/controller/receipt_controller.py
+++ server/controller/receipt_controller.py
@@ -2,11 +2,11 @@ from datetime import date
 
 class ReceiptController:
 
-    def __init__(self, amount, title, receipt_date, pic_url,
-                 category=None, date_created=date.today()):
-        self.amount = amount
-        self.title = title
-        self.receipt_date = receipt_date
-        self.pic_url = pic_url
-        self.category = category
-        self.date_created = date_created
+    def __init__(self, model, view):
+        self.model = model
+        self.view = view
+
+    def create_receipt(self, amount, title, receipt_date, pic_url,
+                       category=None, date_created=date.today()):
+        self.model.create_receipt(amount, title, receipt_date, pic_url,
+                                  category, date_created)
---
+++ server/model/receipt_model.py
@@ -0,0 +1,12 @@
+from datetime import date
+
+class Receipt:
+
+    def __init__(self, amount, title, receipt_date, pic_url,
+                 category=None, date_created=date.today()):
+        self.amount = amount
+        self.title = title
+        self.receipt_date = receipt_date
+        self.pic_url = pic_url
+        self.category = category
+        self.date_created = date_created
