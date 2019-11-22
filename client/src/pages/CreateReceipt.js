8921485942fae817df91d2030f3354cdc0aaca5a
Ethan Low <ethanlow223@yahoo.com>
2019-11-21 22:12:43 -0800

    added create receipt function

--- client/src/pages/CreateReceipt.js
+++ client/src/pages/CreateReceipt.js
@@ -18,18 +18,39 @@ const useStyles = theme => ({
 
 class CreateReceipt extends Component {
   state = {
-    name: '',
-    amount: '',
-    category: ''
+    title: '',
+    amount: 0,
+    category: '',
+    receipt_date: ''
   }
 
   onFormChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value });
+    if (e.target.name === "amount") {
+      let amt = parseFloat(e.target.value);
+      this.setState({ [e.target.name]: amt });
+    } else {
+      this.setState({ [e.target.name]: e.target.value });
+    }
   }
 
   onBtnClick = () => {
     console.log(this.state);
-  }
+    fetch("/receipt", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(this.state)
+    })
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        console.log(data);
+        this.props.history.push("/");
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
 
   render() {
     const { classes } = this.props;
@@ -40,7 +61,7 @@ class CreateReceipt extends Component {
           <Typography variant="h4">Add a Receipt</Typography>
           <Divider />
           <FormControl>
-            <TextField label={"Name"} name={"name"} onChange={this.onFormChange} />
+            <TextField label={"Title"} name={"title"} onChange={this.onFormChange} />
             <TextField label={"Amount"} name={"amount"} onChange={this.onFormChange} />
             <FormControl>
             <InputLabel>Category</InputLabel>
@@ -51,6 +72,7 @@ class CreateReceipt extends Component {
               <MenuItem value={"Other"} >Other</MenuItem>
             </Select>
             </FormControl>
+            <TextField type="date" label={"Purchase Date"} name={"receipt_date"} onChange={this.onFormChange} InputLabelProps={{shrink: true}} />
           </FormControl><br />
           <Button className={classes.button} onClick={this.onBtnClick} >Submit</Button>
         </main>
