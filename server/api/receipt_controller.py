6c40f36e37f25068318979f67926e26f97025200
Isabel Tang <isabeltang220@gmail.com>
2019-11-24 18:51:33 -0500

    Modified receipt controller
    - modified get_all_receipts() in receipt controller to allow users to give optional parameters that filter and sort receipts by month
    - get_all_receipts() now also show the total amount of all receipts shown

--- server/api/receipt_controller.py
+++ server/api/receipt_controller.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, request, flash, jsonify
+from flask import Blueprint, request, jsonify
 from server.models import db, Receipt
+import datetime
+import calendar
 
 receipt_controller = Blueprint('receipt_controller',
                                __name__, url_prefix='/receipts')
@@ -28,17 +30,37 @@ def create():
         failed = True
 
     if failed:
-        return flash("Error: failed to create new receipt")
-    return flash("New receipt created")
+        return jsonify({"Error": "Failed to create new receipt"})
+    return jsonify({"Success": "New receipt created"})
 
 
-@receipt_controller.route('/', methods=['GET'])
-def get_all_receipts():
-    receipts = Receipt.query.all()
-    # Receipt.query.all() returns empty list if there are no records
-    if receipts == []:
-        return receipts
-    return [receipt.to_dict() for receipt in receipts]
+@receipt_controller.route('/', defaults={'year': None, 'month': None},
+                          methods=['GET'])
+@receipt_controller.route('/<int:year>/<int:month>', methods=['GET'])
+def get_all_receipts(year, month):
+    total_amount = 0
+    receipts = []
+
+    if year is None and month is None:
+        receipts = Receipt.query.all()
+        # Receipt.query.all() returns empty list if there are no records
+        if receipts == []:
+            return receipts
+    elif year is not None and month is not None:
+        num_days = calendar.monthrange(year, month)[1]
+        start_date = datetime.date(year, month, 1)
+        end_date = datetime.date(year, month, num_days)
+
+        receipts = (Receipt.query
+                    .filter(Receipt.receipt_date <= end_date)
+                    .filter(Receipt.receipt_date >= start_date)
+                    .order_by(Receipt.receipt_date.desc()))
+
+    for receipt in receipts:
+        total_amount += receipt.amount
+
+    return jsonify({"total_amount": str(total_amount),
+                    "posts": [receipt.to_dict() for receipt in receipts]})
 
 
 @receipt_controller.route('/<int:id>', methods=['GET'])
@@ -69,5 +91,5 @@ def update_receipt(id):
         failed = True
 
     if failed:
-        return flash("Error: failed to update receipt")
-    return flash("Receipt updated")
+        return jsonify({"Error": "Failed to update receipt"})
+    return jsonify({"Success": "Receipt updated"})
--- server/app.py
+++ server/app.py
@@ -3,11 +3,15 @@ from .api.ping_handler import ping_handler
 from .api.home_handler import home_handler
 from .api.receipt_controller import receipt_controller
 from .models import db
+from .config import DB_USERNAME, DB_PASSWORD, DB_NAME
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://{}:{}@localhost/{}'.format(DB_USERNAME, DB_PASSWORD, DB_NAME)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
+
+with app.app_context():
+    db.create_all()
 
 app.register_blueprint(home_handler)
 app.register_blueprint(ping_handler)
