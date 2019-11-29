7f6a623350e7ab9b36bd0931b3711d600eecf76f
William Lee <williamlee021@gmail.com>
2019-11-29 17:29:22 -0500

    Display Receipts in grid using dummy data

--- client/src/pages/Receipts.js
+++ client/src/pages/Receipts.js
@@ -1,6 +1,15 @@
-import React, { Component } from 'react';
-import { Typography, Divider } from '@material-ui/core';
+import React, { Component, useState, useEffect } from 'react';
+import {
+  Typography,
+  Divider,
+  Paper,
+  Grid,
+  Card,
+  CardMedia,
+  CardActionArea
+} from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
+import ReceiptsModal from '../components/ReceiptsModal';
 
 const useStyles = theme => ({
   root: {
@@ -8,22 +17,103 @@ const useStyles = theme => ({
   },
   content: {
     flexGrow: 1,
-    padding: theme.spacing(0, 40),
+    padding: theme.spacing(0, 40)
   },
   toolbar: theme.mixins.toolbar
 });
 
-class Receipts extends Component {
-  render() {
-    const { classes } = this.props;
-    return (
-      <div className={classes.root}>
-        <Typography variant="h4">Receipts</Typography>
-        <Divider />
-        <Typography paragraph>SOME COOL STUFF HERE</Typography>
-      </div>
-    );
-  }
-}
+const Receipts = props => {
+  const { classes } = props;
+  const [data, setData] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [modalData, setModalData] = useState({});
+
+  // useEffect(() => {
+  //   console.log('in use effect');
+  //   fetch('/receipts', {
+  //     method: 'GET',
+  //     headers: {
+  //       'Content-Type': 'application/json'
+  //     }
+  //   })
+  //     .then(response => {
+  //       return response.json();
+  //     })
+  //     .then(text => {
+  //       console.log(text);
+  //     });
+  // }, []);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  console.log(props);
+
+  return (
+    <div className={classes.root}>
+      <Typography variant='h4'>Receipts</Typography>
+      <Divider />
+      <ReceiptsModal
+        handleClose={handleClose}
+        open={open}
+        message={'Testhing one two three'}
+      />
+      <Paper>
+        <Grid container direction='row' spacing={3}>
+          <Grid item md={4}>
+            <Card>
+              <CardActionArea>
+                <CardMedia
+                  image='https://source.unsplash.com/random'
+                  style={{ height: '100px' }}
+                  // onclick it will set its info with setModalInfo
+                ></CardMedia>
+                <Typography>This is my image</Typography>
+              </CardActionArea>
+            </Card>
+          </Grid>
+          <Grid item md={4}>
+            <Card>
+              <CardActionArea>
+                <CardMedia
+                  image='https://source.unsplash.com/random'
+                  style={{ height: '100px' }}
+                ></CardMedia>
+                <Typography>This is my image</Typography>
+              </CardActionArea>
+            </Card>
+          </Grid>
+          <Grid item md={4}>
+            <Card>
+              <CardActionArea>
+                <CardMedia
+                  image='https://source.unsplash.com/random'
+                  style={{ height: '100px' }}
+                ></CardMedia>
+                <Typography>This is my image</Typography>
+              </CardActionArea>
+            </Card>
+          </Grid>
+          <Grid item md={4}>
+            <Card>
+              <CardActionArea>
+                <CardMedia
+                  image='https://source.unsplash.com/random'
+                  style={{ height: '100px' }}
+                ></CardMedia>
+                <Typography>This is my image</Typography>
+              </CardActionArea>
+            </Card>
+          </Grid>
+        </Grid>
+      </Paper>
+    </div>
+  );
+};
 
 export default withStyles(useStyles)(Receipts);
