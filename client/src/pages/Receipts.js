9a2e65ef889017c255a408915b9d3bb42adf9266
William Lee <williamlee021@gmail.com>
2019-11-30 03:17:57 -0500

    Add month select to Receipt

--- client/src/pages/Receipts.js
+++ client/src/pages/Receipts.js
@@ -6,10 +6,14 @@ import {
   Grid,
   Card,
   CardMedia,
-  CardActionArea
+  CardActionArea,
+  FormControl,
+  MenuItem,
+  Select
 } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
 import ReceiptsModal from '../components/ReceiptsModal';
+import ReceiptsSelect from '../components/ReceiptsSelect';
 
 const useStyles = theme => ({
   root: {
@@ -27,6 +31,20 @@ const Receipts = props => {
   const [data, setData] = useState([]);
   const [open, setOpen] = useState(false);
   const [modalData, setModalData] = useState({});
+  const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+  ];
 
   // useEffect(() => {
   //   console.log('in use effect');
@@ -56,62 +74,69 @@ const Receipts = props => {
 
   return (
     <div className={classes.root}>
-      <Typography variant='h4'>Receipts</Typography>
+      <Grid container alignItems='center'>
+        <Grid item sm>
+          <Typography variant='h4'>Receipts</Typography>
+        </Grid>
+        <Grid item>
+          <ReceiptsSelect months={months} />
+        </Grid>
+      </Grid>
       <Divider />
       <ReceiptsModal
         handleClose={handleClose}
         open={open}
         message={'Testhing one two three'}
       />
-      <Paper>
-        <Grid container direction='row' spacing={3}>
-          <Grid item md={4}>
-            <Card>
-              <CardActionArea>
-                <CardMedia
-                  image='https://source.unsplash.com/random'
-                  style={{ height: '100px' }}
-                  // onclick it will set its info with setModalInfo
-                ></CardMedia>
-                <Typography>This is my image</Typography>
-              </CardActionArea>
-            </Card>
-          </Grid>
-          <Grid item md={4}>
-            <Card>
-              <CardActionArea>
-                <CardMedia
-                  image='https://source.unsplash.com/random'
-                  style={{ height: '100px' }}
-                ></CardMedia>
-                <Typography>This is my image</Typography>
-              </CardActionArea>
-            </Card>
-          </Grid>
-          <Grid item md={4}>
-            <Card>
-              <CardActionArea>
-                <CardMedia
-                  image='https://source.unsplash.com/random'
-                  style={{ height: '100px' }}
-                ></CardMedia>
-                <Typography>This is my image</Typography>
-              </CardActionArea>
-            </Card>
-          </Grid>
-          <Grid item md={4}>
-            <Card>
-              <CardActionArea>
-                <CardMedia
-                  image='https://source.unsplash.com/random'
-                  style={{ height: '100px' }}
-                ></CardMedia>
-                <Typography>This is my image</Typography>
-              </CardActionArea>
-            </Card>
-          </Grid>
+      {/* <Paper> */}
+      <Grid container direction='row' spacing={3}>
+        <Grid item md={4}>
+          <Card>
+            <CardActionArea>
+              <CardMedia
+                image='https://source.unsplash.com/random'
+                style={{ height: '100px' }}
+                // onclick it will set its info with setModalInfo
+              ></CardMedia>
+              <Typography>This is my image</Typography>
+            </CardActionArea>
+          </Card>
         </Grid>
-      </Paper>
+        <Grid item md={4}>
+          <Card>
+            <CardActionArea>
+              <CardMedia
+                image='https://source.unsplash.com/random'
+                style={{ height: '100px' }}
+              ></CardMedia>
+              <Typography>This is my image</Typography>
+            </CardActionArea>
+          </Card>
+        </Grid>
+        <Grid item md={4}>
+          <Card>
+            <CardActionArea>
+              <CardMedia
+                image='https://source.unsplash.com/random'
+                style={{ height: '100px' }}
+              ></CardMedia>
+              <Typography>This is my image</Typography>
+            </CardActionArea>
+          </Card>
+        </Grid>
+        <Grid item md={4}>
+          <Card>
+            <CardActionArea>
+              <CardMedia
+                image='https://source.unsplash.com/random'
+                style={{ height: '100px' }}
+              ></CardMedia>
+              <Typography>This is my image</Typography>
+            </CardActionArea>
+          </Card>
+        </Grid>
+      </Grid>
+      {/* </Paper> */}
     </div>
   );
 };
