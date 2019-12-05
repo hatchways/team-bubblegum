810e3d6e71d5a2ed0ac42e6f7334295cce6667fd
Isabel Tang <isabeltang220@gmail.com>
2019-12-05 11:02:22 -0500

    Removed print statements

--- server/api/tasks.py
+++
@@ -1,9 +0,0 @@
-import users as usr
-import emails as eml
-
-def send_monthly_overview_emails():
-    all_users = usr.get_all_users().json['users']
-    for user in all_users:
-        user_id = user['id']
-        user_email = user['email']
-        eml.end_of_month_overview(user_id, user_email)
